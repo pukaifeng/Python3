3343485e6cff5e0c8491b83321cf35b3ccab1618
话秋凉 <33722039+pukaifeng@users.noreply.github.com>
2018-03-13 18:11:08 +0800

    用sorted()函数实现各种排序

---
+++ code/test_sorted.py
@@ -0,0 +1,6 @@
+#用sorted()函数实现各种排序
+students = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+
+print(sorted(students, key=itemgetter(0)))
+print(sorted(students, key=lambda t: t[1]))
+print(sorted(students, key=itemgetter(1), reverse=True))
