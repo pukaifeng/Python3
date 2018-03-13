f46dd29f7939a30f1623eba28a620229251dda69
话秋凉 <33722039+pukaifeng@users.noreply.github.com>
2018-03-13 17:55:40 +0800

    Create str2num.py

---
+++ code/str2num.py
@@ -0,0 +1,16 @@
+#用map()和reduce()实现字符串转数字
+def add(x,y):
+    return x*10+y
+
+print(reduce(add,[1,3,5,7,9]))
+
+DIGITS = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+
+def str2num(s):
+    def fn(x,y):
+        return x*10+y
+    def char2nub(s):
+        return DIGITS[s]
+    return reduce(fn,map(char2nub,s))
+
+print(str2num('123456789'))
