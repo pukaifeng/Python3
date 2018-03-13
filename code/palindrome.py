091ae990c82b8bbb29f1f127653db9aeaadc6422
话秋凉 <33722039+pukaifeng@users.noreply.github.com>
2018-03-13 18:14:38 +0800

    用filter()函数求回数

--- code/palindrome.py
+++ code/palindrome.py
@@ -1,6 +1,6 @@
 #用filter()函数求回数
 def is_palindrome(n):  
-    return str(n)==str(n)[::-1]  
+    return str(n) == str(n)[::-1]  
   
 output = filter(is_palindrome, range(1, 100))  
 print(list(output))
