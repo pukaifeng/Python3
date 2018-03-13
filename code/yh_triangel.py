b1bd33bd276f2033d898694ecc451f85fe0fd590
话秋凉 <33722039+pukaifeng@users.noreply.github.com>
2018-03-13 18:18:42 +0800

    打印杨辉三角

--- code/yh_triangel.py
+++ code/yh_triangel.py
@@ -4,7 +4,7 @@ def yh_triangel(n):
     while True:
         yield L                                         #打印出该list
         L=[L[x]+L[x+1] for x in range(len(L)-1)]        #计算下一行中间的值（除去两边的1）
-        L.insert(0,1)                                   #在开头插入1
+        L.insert(0, 1)                                  #在开头插入1
         L.append(1)                                     #在结尾添加1
         if len(L)>10:                                   #仅输出10行
             break
